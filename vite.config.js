611542ad0e5d5f4dd6ecd19969eb8aeecb5d3dcf
JohnPauloGH <johnpaulobautista014@gmail.com>
2026-05-22 13:33:47 +0800

    ngrok

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve("./src"),
     },
+  },
+
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "pregame-custodian-munchkin.ngrok-free.dev",
+    ],
   },
 })
